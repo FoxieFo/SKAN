0409046de4236b50814117b2b59dcb279f1f182b
victoria <itsfoxiefo@gmail.com>
2024-10-24 23:44:26 +0200

    tuned AuthForm

--- src/components/AuthForm/AuthForm.jsx
+++ src/components/AuthForm/AuthForm.jsx
@@ -11,15 +11,50 @@ export default function AuthForm() {
     return (
         <section className={s.authform__container}>
             <div className={s.authform__info}>
-                <p className={s.authform__infoText}>Для оформления подписки на тариф, необходимо авторизоваться.</p>
-                <img src={Heroes} alt="heroes" />
+                <p className={s.authform__infoText}>
+                    Для оформления подписки на тариф, необходимо авторизоваться.
+                </p>
+                <img src={Heroes} alt="heroes" className={s.authform__heroesImg} />
             </div>
+
             <div className={s.authform__form}>
-                <div className={s.authform__buttons}>
-                    <button>Войти</button>
-                    <button>Зарегистрироваться</button>
+                <div className={s.authform__formBtn}>
+                    <button className={s.authform__formBtnLogin}>Войти</button>
+                    <button className={s.authform__formBtnRegister}>Зарегистрироваться</button>
                 </div>
-                <form className={s.authform__inputs}></form>
+
+                <form className={s.authform__formInputs}>
+                    <label htmlFor="login" className={s.authform__formInputsLabel}>Логин или номер телефона:</label>
+                    <input
+                        id="login"
+                        type="text"
+                        className={s.authform__formInputsItem}
+                        placeholder="Введите логин или телефон"
+                    />
+
+                    <label htmlFor="password" className={s.authform__formInputsLabel}>Пароль:</label>
+                    <input
+                        id="password"
+                        type="password"
+                        className={s.authform__formInputsItem}
+                        placeholder="Введите пароль"
+                    />
+
+                    <button type="submit" className={s.authform__formSubmit}>Войти</button>
+                </form>
+
+                <button className={s.authform__formRecover}>Восстановить пароль</button>
+
+                <div className={s.authform__formSocial}>
+                    <span className={s.authform__formSocialLabel}>Войти через:</span>
+                    <div className={s.authform__formSocialIcons}>
+                        <img src={Google} alt="Google" className={s.authform__formSocialIconsItem} />
+                        <img src={Facebook} alt="Facebook" className={s.authform__formSocialIconsItem} />
+                        <img src={Yandex} alt="Yandex" className={s.authform__formSocialIconsItem} />
+                    </div>
+                </div>
+
+                <img src={Padlock} alt="Padlock" className={s.authform__img} />
             </div>
         </section>
     );
--- src/pages/AuthPage/AuthPage.jsx
+++ src/pages/AuthPage/AuthPage.jsx
@@ -1,16 +1,16 @@
 import Footer from '../../components/Footer/Footer';
 import Header from '../../components/Header/Header';
-import AuthForm from '../../components/AuthForm/AuthForm'
-import s from './styles.module.scss'
+import AuthForm from '../../components/AuthForm/AuthForm';
+import s from './styles.module.scss';
 
 export default function AuthPage() {
     return (
-        <>
+        <div className={s.page}>
             <Header />
             <main>
                 <AuthForm />
             </main>
             <Footer />
-        </>
+        </div>
     );
 }
