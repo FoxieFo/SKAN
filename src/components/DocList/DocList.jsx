4567e0eae08414c6cc9c600d42753f197fe5ea85
victoria <itsfoxiefo@gmail.com>
2024-11-05 16:40:45 +0100

    obj in DocList

--- src/components/DocList/DocList.jsx
+++ src/components/DocList/DocList.jsx
@@ -1,7 +1,8 @@
-import s from './styles.module.scss'
+import s from './styles.module.scss';
 import { useSelector } from "react-redux";
-import DocCard from './../ui/DocCard/DocCard'
-import { useState } from 'react';
+import DocCard from './../ui/DocCard/DocCard';
+import { useState, useEffect } from 'react';
+import PostService from './../../services/PostService';
 
 export default function DocList() {
     const publicationsList = useSelector(state => state.publications.publicationsList);
@@ -11,17 +12,46 @@ export default function DocList() {
 
     console.log("allPublications:", allPublications);
 
-    const [showCard, setShowCard] = useState(1);
+    const [loadedDocs, setLoadedDocs] = useState([]);
+    const [error, setError] = useState(false);
+    const [showCard, setShowCard] = useState(2);
+
+    function fetchDocuments(indexOf, count = 10) {
+        if (indexOf >= allPublications.length) {
+            return;
+        }
+
+        PostService.getDocuments(allPublications.slice(indexOf, indexOf + count).map(x => x.encodedId))
+            .then(response => {
+                console.log(response);
+                setLoadedDocs(prevDocs => [...prevDocs, ...response.data]);
+                setError(false);
+            })
+            .catch(() => {
+                setLoadedDocs([]);
+                setError(true);
+            });
+    }
+
+    useEffect(() => {
+        fetchDocuments(0, showCard);
+    }, []);
+
+    const handleShowMore = () => {
+        setShowCard(prev => prev + 1);
+        fetchDocuments(showCard, 1);
+    };
 
     return (
         <section className={s.doclist__container}>
             <h3 className={s.doclist__heading}>Список документов</h3>
             <div>
-                {allPublications.slice(0, showCard).map((item) => (
-                    <DocCard key={item.id} data={item} />
+                {loadedDocs.map((item) => (
+                    <DocCard key={item.ok.id} data={item.ok} />
                 ))}
             </div>
-            <button onClick={() => setShowCard((prev) => prev + 1)}>Показать больше</button>
+            <button onClick={handleShowMore}>Показать больше</button>
+            {error && <p>Произошла ошибка при загрузке документов.</p>}
         </section>
     );
 }
--- src/components/ui/DocCard/DocCard.jsx
+++ src/components/ui/DocCard/DocCard.jsx
@@ -1,78 +1,77 @@
 import s from './styles.module.scss'
 import HTMLReactParser from "html-react-parser";
 
-function formatContent(inputMarkup) {
-    let formattedMarkup = inputMarkup.replaceAll("&lt;", "<")
-        .replaceAll("&gt;", ">");
+// function formatContent(inputMarkup) {
+//     let formattedMarkup = inputMarkup.replaceAll("&lt;", "<")
+//         .replaceAll("&gt;", ">");
 
-    formattedMarkup = formattedMarkup.replaceAll("<scandoc>", "<section>")
-        .replaceAll("</scandoc>", "</section>")
-        .replaceAll("<p>", "<span>")
-        .replaceAll("</p>", "</span>")
-        .replaceAll("<sentence>", "<p>")
-        .replaceAll("</sentence>", "</p>")
-        .replaceAll("<entity", "<span")
-        .replaceAll("</entity>", "</span>")
-        .replaceAll("<speech", "<span")
-        .replaceAll("</speech>", "</span>")
-        .replaceAll("<vue", "<span")
-        .replaceAll("</vue>", "</span>")
-        .replaceAll("<br>", "");
+//     formattedMarkup = formattedMarkup.replaceAll("<scandoc>", "<section>")
+//         .replaceAll("</scandoc>", "</section>")
+//         .replaceAll("<p>", "<span>")
+//         .replaceAll("</p>", "</span>")
+//         .replaceAll("<sentence>", "<p>")
+//         .replaceAll("</sentence>", "</p>")
+//         .replaceAll("<entity", "<span")
+//         .replaceAll("</entity>", "</span>")
+//         .replaceAll("<speech", "<span")
+//         .replaceAll("</speech>", "</span>")
+//         .replaceAll("<vue", "<span")
+//         .replaceAll("</vue>", "</span>")
+//         .replaceAll("<br>", "");
 
-    while (formattedMarkup.includes("<figure>")) {
-        let figureHTML = formattedMarkup.substring(formattedMarkup.indexOf("<figure>"));
-        figureHTML = figureHTML.substring(0, figureHTML.indexOf("</figure>") + "</figure>".length);
+//     while (formattedMarkup.includes("<figure>")) {
+//         let figureHTML = formattedMarkup.substring(formattedMarkup.indexOf("<figure>"));
+//         figureHTML = figureHTML.substring(0, figureHTML.indexOf("</figure>") + "</figure>".length);
 
-        if (!figureHTML.includes("data-image-src")) {
-            formattedMarkup = formattedMarkup.replace(figureHTML, "");
-            continue;
-        }
+//         if (!figureHTML.includes("data-image-src")) {
+//             formattedMarkup = formattedMarkup.replace(figureHTML, "");
+//             continue;
+//         }
 
-        let url = figureHTML.substring(
-            figureHTML.indexOf("data-image-src") + 'data-image-src="'.length
-        );
-        url = url.substring(0, url.indexOf('"'));
+//         let url = figureHTML.substring(
+//             figureHTML.indexOf("data-image-src") + 'data-image-src="'.length
+//         );
+//         url = url.substring(0, url.indexOf('"'));
 
-        formattedMarkup = formattedMarkup.replace(
-            figureHTML,
-            `<img src="${url}" alt="Фото из публикации">`
-        );
-    }
+//         formattedMarkup = formattedMarkup.replace(
+//             figureHTML,
+//             `<img src="${url}" alt="Фото из публикации">`
+//         );
+//     }
 
-    while (formattedMarkup.includes("<span></span>")) {
-        formattedMarkup = formattedMarkup.replace("<span></span>", "");
-    }
+//     while (formattedMarkup.includes("<span></span>")) {
+//         formattedMarkup = formattedMarkup.replace("<span></span>", "");
+//     }
 
-    while (formattedMarkup.includes("<p></p>")) {
-        formattedMarkup = formattedMarkup.replace("<p></p>", "");
-    }
+//     while (formattedMarkup.includes("<p></p>")) {
+//         formattedMarkup = formattedMarkup.replace("<p></p>", "");
+//     }
 
-    if (formattedMarkup.length > 1800) {
-        formattedMarkup = formattedMarkup.substring(
-            0,
-            formattedMarkup.lastIndexOf("</p>", 1700) + "</p>".length
-        );
-        formattedMarkup += "...";
-    }
+//     if (formattedMarkup.length > 1800) {
+//         formattedMarkup = formattedMarkup.substring(
+//             0,
+//             formattedMarkup.lastIndexOf("</p>", 1700) + "</p>".length
+//         );
+//         formattedMarkup += "...";
+//     }
 
-    return formattedMarkup;
-}
+//     return formattedMarkup;
+// }
 
 export default function DocCard({ data }) {
-    
     return (
         <article className={s.doccard__container}>
             <div className={s.doccard__heading}>
                 <div className={s.doccard__headingDate}></div>
-                <div className={s.doccard__headingLink}></div>
+                <a className={s.doccard__headingLink}></a>
             </div>
-            <h3 className={s.doccard__title}></h3>
+            <h3 className={s.doccard__title}>{data.title.text}</h3>
             <p className={s.doccard__type}></p>
             <img className={s.doccard__img}/>
             <p className={s.doccard__content}></p>
             <div className={s.doccard__footer}>
                 <button className={s.doccard__footerBtn}>Читать в источнике</button>
-                <span className={s.doccard__footerCount}></span>
+                <span className={s.doccard__footerCount}>{data.attributes.wordCount}</span>
             </div>
         </article>
     );
