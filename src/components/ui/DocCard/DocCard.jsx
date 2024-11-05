bbfac515da57f0dec9906631126dc583fad6f8d2
victoria <itsfoxiefo@gmail.com>
2024-11-05 23:34:37 +0100

    DocCard ready

--- src/components/ui/DocCard/DocCard.jsx
+++ src/components/ui/DocCard/DocCard.jsx
@@ -1,81 +1,77 @@
-import s from './styles.module.scss'
+import s from './styles.module.scss';
 import HTMLReactParser from "html-react-parser";
+import articleImg from './../../../assets/images/article-img.png';
 
-// function formatContent(inputMarkup) {
-//     let formattedMarkup = inputMarkup.replaceAll("&lt;", "<")
-//         .replaceAll("&gt;", ">");
+function formatContent(inputMarkup) {
+    let formattedMarkup = inputMarkup
+        .replaceAll("&lt;", "<")
+        .replaceAll("&gt;", ">")
+        .replaceAll("<scandoc>", "<section>")
+        .replaceAll("</scandoc>", "</section>")
+        .replaceAll("<p>", "<span>")
+        .replaceAll("</p>", "</span>")
+        .replaceAll("<sentence>", "<p>")
+        .replaceAll("</sentence>", "</p>")
+        .replaceAll("<entity", "<span")
+        .replaceAll("</entity>", "</span>")
+        .replaceAll("<speech", "<span")
+        .replaceAll("</speech>", "</span>")
+        .replaceAll("<vue", "<span")
+        .replaceAll("</vue>", "</span>")
+        .replaceAll("<br>", "");
 
-//     formattedMarkup = formattedMarkup.replaceAll("<scandoc>", "<section>")
-//         .replaceAll("</scandoc>", "</section>")
-//         .replaceAll("<p>", "<span>")
-//         .replaceAll("</p>", "</span>")
-//         .replaceAll("<sentence>", "<p>")
-//         .replaceAll("</sentence>", "</p>")
-//         .replaceAll("<entity", "<span")
-//         .replaceAll("</entity>", "</span>")
-//         .replaceAll("<speech", "<span")
-//         .replaceAll("</speech>", "</span>")
-//         .replaceAll("<vue", "<span")
-//         .replaceAll("</vue>", "</span>")
-//         .replaceAll("<br>", "");
+    // Replace <figure> elements containing images with <img> elements or remove them if missing src
+    formattedMarkup = formattedMarkup.replace(/<figure>(.*?)<\/figure>/g, (match) => {
+        const srcMatch = match.match(/data-image-src="([^"]+)"/);
+        return srcMatch ? `<img src="${srcMatch[1]}" alt="Фото из публикации">` : "";
+    });
 
-//     while (formattedMarkup.includes("<figure>")) {
-//         let figureHTML = formattedMarkup.substring(formattedMarkup.indexOf("<figure>"));
-//         figureHTML = figureHTML.substring(0, figureHTML.indexOf("</figure>") + "</figure>".length);
+    // Remove empty <span> and <p> elements
+    formattedMarkup = formattedMarkup.replace(/<span><\/span>|<p><\/p>/g, "");
 
-//         if (!figureHTML.includes("data-image-src")) {
-//             formattedMarkup = formattedMarkup.replace(figureHTML, "");
-//             continue;
-//         }
+    // Limit content length and add ellipsis if it exceeds 1800 characters
+    if (formattedMarkup.length > 1800) {
+        formattedMarkup = formattedMarkup.slice(0, formattedMarkup.lastIndexOf("</p>", 1700) + 4) + "...";
+    }
 
-//         let url = figureHTML.substring(
-//             figureHTML.indexOf("data-image-src") + 'data-image-src="'.length
-//         );
-//         url = url.substring(0, url.indexOf('"'));
-
-//         formattedMarkup = formattedMarkup.replace(
-//             figureHTML,
-//             `<img src="${url}" alt="Фото из публикации">`
-//         );
-//     }
-
-//     while (formattedMarkup.includes("<span></span>")) {
-//         formattedMarkup = formattedMarkup.replace("<span></span>", "");
-//     }
-
-//     while (formattedMarkup.includes("<p></p>")) {
-//         formattedMarkup = formattedMarkup.replace("<p></p>", "");
-//     }
-
-//     if (formattedMarkup.length > 1800) {
-//         formattedMarkup = formattedMarkup.substring(
-//             0,
-//             formattedMarkup.lastIndexOf("</p>", 1700) + "</p>".length
-//         );
-//         formattedMarkup += "...";
-//     }
-
-//     return formattedMarkup;
-// }
+    return formattedMarkup;
+}
 
 export default function DocCard({ data }) {
+    const formattedText = HTMLReactParser(formatContent(data.content.markup), { replace: "text/xml" });
+    const typeText = data.attributes.isTechNews
+        ? "Техновости"
+        : data.attributes.isAnnouncement
+        ? "Анонсы и события"
+        : data.attributes.isDigest
+        ? "Сводки новостей"
+        : "Общие новости";
+
+    const imageSrc = data.content.markup.match(/<img src="([^"]+)"/)?.[1] || articleImg;
+
     return (
         <article className={s.doccard__container}>
             <div className={s.doccard__heading}>
                 <div className={s.doccard__headingDate}>{new Date(data.issueDate).toLocaleDateString()}</div>
-                <a href={data.url} target="_blank" rel="noreferrer" className={s.doccard__headingLink}><span className={s.doccard__headingLinkSource}>{data.source.name}</span></a>
+                <a href={data.url} target="_blank" rel="noreferrer" className={s.doccard__headingLink}>
+                    <span className={s.doccard__headingLinkSource}>{data.source.name}</span>
+                </a>
             </div>
-            <h3 className={s.doccard__title}>{data.title.text}</h3>
-            <p className={s.doccard__type}>
-                
-            </p>
-            <img className={s.doccard__img} />
-            <p className={s.doccard__content}></p>
+            <h3 className={s.doccard__title}>
+                {data.title.text.length > 60 ? `${data.title.text.slice(0, 60)}...` : data.title.text}
+            </h3>
+            <p className={s.doccard__type}>{typeText}</p>
+            <img
+                className={s.doccard__img}
+                src={imageSrc}
+                alt="Фото из публикации"
+                onError={(e) => { e.target.onerror = null; e.target.src = articleImg; }}
+            />
+            <p className={s.doccard__content}>{formattedText}</p>
             <div className={s.doccard__footer}>
                 <button className={s.doccard__footerBtn}>
-                    <a href={data.url} target="_blank" rel="noreferrer">
-                        Читать в источнике
-                    </a></button>
+                    <a href={data.url} target="_blank" rel="noreferrer">Читать в источнике</a>
+                </button>
                 <span className={s.doccard__footerCount}>{data.attributes.wordCount} слов</span>
             </div>
         </article>
