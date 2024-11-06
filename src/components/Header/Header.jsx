dd994efda410d424e17ecad8666b85b936fdabb9
victoria <itsfoxiefo@gmail.com>
2024-11-06 01:11:50 +0100

    responsive: Header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,19 +1,29 @@
 import { Link } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import useWindowWidth from './../../hooks/useWindowWidth';
+import { useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import Offcanvas from 'react-bootstrap/Offcanvas';
 
 import s from './styles.module.scss';
 
 import Logo from './../../assets/images/icons/logo.svg';
+import LogoBurger from './../../assets/images/icons/logo-burger.svg'
 import Line from './../../assets/images/icons/line.svg';
+import CloseIcon from './../../assets/images/icons/close-burger.svg'
 
-import Button from './../ui/Button/Button';
-import Profile from './../ui/Profile/Profile'
+import ButtonCstm from './../ui/Button/Button';
+import Profile from './../ui/Profile/Profile';
 import CompanyCount from '../ui/CompanyCount/CompnayCount';
 
 export default function Header() {
     const isAuthenticated = useSelector((state) => state.auth?.isAuth || false);
     const windowWidth = useWindowWidth();
+
+    const [showOffcanvas, setShowOffcanvas] = useState(false);
+
+    const handleClose = () => setShowOffcanvas(false);
+    const handleShow = () => setShowOffcanvas(true);
 
     return (
         <header className={s.header__container}>
@@ -32,11 +42,41 @@ export default function Header() {
                 </li>
             </ul>
             <div className={s.header__profile}>
-            {isAuthenticated ? (
+                {isAuthenticated ? (
                     windowWidth < 840 ? (
-                        <div className={s.header__profileLoggedinBurger}>
+                        <div className={s.header__profileLoggedin}>
                             <CompanyCount />
-                            <div className={s.burger}></div>
+                            <div className={s.header__hamburger}>
+                                <Button variant="primary" onClick={handleShow}>
+                                    <div className={s.header__hamburgerLines}>
+                                        <span className={s.header__hamburgerLinesLine}></span>
+                                        <span className={s.header__hamburgerLinesLine}></span>
+                                        <span className={s.header__hamburgerLinesLine}></span>
+                                    </div>
+                                </Button>
+                                <Offcanvas show={showOffcanvas} onHide={handleClose} placement="end">
+                                    <div className={s.header__hamburgerOffcanvas}>
+                                        <Offcanvas.Header closeButton>
+                                            <img src={LogoBurger} alt="logo" />
+                                        </Offcanvas.Header>
+                                        <Offcanvas.Body>
+                                            <ul className={s.header__hamburgerOffcanvasOptions}>
+                                                <li className={s.header__hamburgerOffcanvasOptionsOption}>
+                                                    <Link to="/">Главная</Link>
+                                                </li>
+                                                <li className={s.header__hamburgerOffcanvasOptionsOption}>
+                                                    <Link to="/">Тарифы</Link>
+                                                </li>
+                                                <li className={s.header__hamburgerOffcanvasOptionsOption}>
+                                                    <Link to="/">FAQ</Link>
+                                                </li>
+                                            </ul>
+
+
+                                        </Offcanvas.Body>
+                                    </div>
+                                </Offcanvas>
+                            </div>
                         </div>
                     ) : (
                         <div className={s.header__profileLoggedin}>
@@ -49,7 +89,7 @@ export default function Header() {
                         <span className={s.header__profileLoginReg}>Зарегистрироваться</span>
                         <img className={s.header__line} src={Line} alt="line" />
                         <Link>
-                            <Button title={'Войти'} colorScheme={'colorScheme__2'} to={'/auth'}/>
+                            <ButtonCstm title={'Войти'} colorScheme={'colorScheme__2'} to={'/auth'} />
                         </Link>
                     </div>
                 )}
--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,5 @@
 import { createRoot } from 'react-dom/client'
+import 'bootstrap/dist/css/bootstrap.min.css';
 import { Provider } from 'react-redux';
 import store from './store/store.js';
 import App from './App.jsx'
